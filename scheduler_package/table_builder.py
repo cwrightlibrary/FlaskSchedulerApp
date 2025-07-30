0b180b23c3ec8f9c973b01821bd72244044ad6d4
Chris Wright <cwright@richlandlibrary.com>
2025-07-30 10:57:51 -0400

    updating

--- scheduler_package/table_builder.py
+++ scheduler_package/table_builder.py
@@ -7,13 +7,13 @@ class TableBuilder:
         self.order = ["header"]
 
         self.minimum_width = 10
+        self.maximum_width = 20
     
     def set_title(self, title: str):
         self.title = title
     
     def set_headers(self, headers: list):
         self.headers = [headers]
-        self.order.append("header")
     
     def add_row(self, row: list, section: bool = False):
         if section:
@@ -35,51 +35,22 @@ class TableBuilder:
     def set_minimum_width(self, width: int):
         self.minimum_width = width
     
+    def set_maximum_width(self, width: int):
+        self.maximum_width = width
+    
     def build_table(self):
+        widths_rows = self.headers + self.sections + self.cells
+        widths_num_cols = len(self.headers[0])
+
         widths = []
-        for idx in range(len(self.headers)):
-            col_idx = []
-            for col in range(len(self.headers[idx])):
-                current_width = self.minimum_width
-                if len(self.headers[idx][col]) > current_width:
-                    current_width = len(self.headers[idx][col])
-                if len(self.cells[idx][col]) > current_width:
-                    current_width = len(self.cells[idx][col])
-                widths.append(current_width)
-            # for col in range(len(self.headers[idx])):
-            #     current_col = self.minimum_width
-            #     if len(self.headers[idx][col]) > current_col:
-            #         current_col = len(self.headers[idx][col])
-            #     if len(self.cells[idx][col]) > current_col:
-            #         current_col = len(self.cells[idx][col])
-            #     widths.append(current_col)
+        for col in range(widths_num_cols):
+            max_len = max((len(row[col]) for row in widths_rows if col < len(row)), default=0)
+            widths.append(max(max_len + 2, self.minimum_width))
         
         all_rows = []
-        # for row in range(len(self.headers)):
-        #     if len(self.headers[row]) == 1:
-        #         pass
-        #     else:
-        #         top_mid_bot = ["", "", ""]
-        #         for column in range(len(self.headers[row])):               
-        #             if column == 0:
-        #                 self._create_row(top_mid_bot, self.headers[row][column], self.minimum_width, "start", "header")
-        #             if column > 0 and column < len(self.headers[row]) - 1:
-
-        #                 self._create_row(top_mid_bot, self.headers[row][column], self.minimum_width, "mid", "header")
-        #             if column == len(self.headers[row]) - 1:
-        #                 self._create_row(top_mid_bot, self.headers[row][column], self.minimum_width, "end", "header")
-        #         all_rows.append(top_mid_bot)
-        #         print("\n".join(all_rows[0]))
         for idx, instance in enumerate(self.order):
             last_cell, current_cell, next_cell = "", "", ""
             top_mid_bot = ["", "", ""]
-
-            # current_cell = instance
-            # if idx == 0:
-            #     next_cell = self.order[idx + 1]
-            # elif idx > 0 and idx < len(self.order) - 1:
-            #     last_cell = self.order[idx - 1]
-            #     next_cell = self.order[idx + 1]
             
             if idx == 0:
                 last_cell = ""
@@ -93,22 +64,18 @@ class TableBuilder:
                 last_cell = self.order[idx - 1]
                 current_cell = self.order[idx]
                 next_cell = ""
-            
-            # cell_type = self._get_cell_type(last_cell, current_cell, next_cell)
-            if current_cell == "header" and next_cell == "section":
-                cell_type = "header_section_bot"
-            if current_cell == "section" and next_cell == "cell":
-                cell_type = "section_header_top"
-            if current_cell == "section" and last_cell == "cell":
-                cell_type = "section_cell_top"
-            if current_cell == "cell" and last_cell == "section":
-                cell_type = "cell_section_top"
-            if current_cell == "cell" and next_cell == "section":
-                cell_type = "cell_section_bot"
-            if current_cell == "cell" and next_cell == "cell":
-                cell_type = "cell_cell_bot"
-            if current_cell == "cell" and next_cell == "":
-                cell_type = "cell_end"
+
+            if current_cell == "header":
+                cell_type = "header"
+            elif current_cell == "section":
+                cell_type = "section"
+            elif current_cell == "cell" and next_cell == "cell":
+                cell_type = "cell_a"
+            elif current_cell == "cell" and next_cell == "section":
+                cell_type = "cell_b"
+            elif current_cell == "cell" and next_cell == "":
+                cell_type = "cell_c"
+
 
             if instance == "header":
                 column_data = self.headers[idx]
@@ -124,13 +91,23 @@ class TableBuilder:
                     loc = "mid"
                 elif column == len(column_data) - 1:
                     loc = "end"
-                self._create_row(top_mid_bot, column_data[column], self.minimum_width, loc, cell_type)
+                # if cell_type == "section":
+                #     print(loc)
+                self._create_row(top_mid_bot, column_data[column], widths[column], loc, cell_type)
             
+            if cell_type != "header":
+                top_mid_bot = [top_mid_bot[1], top_mid_bot[2]]
             all_rows.append(top_mid_bot)
-            print("\n".join(top_mid_bot))
+        
+        title_text = self.title
+        full_width = len("\n".join(all_rows[0]).split("\n")[0])
+        self.title = self._center_text(title_text, full_width)
 
+        full_string = f"{self.title}\n"
+        for row in all_rows:
+            full_string += f"{'\n'.join(row)}\n"
 
-        return ""
+        return full_string
 
     def _center_text(self, text, size):
         if len(text) > size:
@@ -140,103 +117,73 @@ class TableBuilder:
         return " " * left + text + " " * right
     
     def _create_border(self, text: str, size: int, pos: str, instance: str):
-        header_only = {
-            "topl": "┏", "topr": "┓", "topsp": "┳",
-            "botl": "┗", "botr": "┛", "botsp": "┻",
-            "horz": "━", "vert": "┃"
-        }
-        header_section_bot = {
+        header = {
             "topl": "┏", "topr": "┓", "topsp": "┳",
             "botl": "┣", "botr": "┫", "botsp": "┻",
             "horz": "━", "vert": "┃"
         }
-        header_cell_bot = {
-            "topl": "┏", "topr": "┓", "topsp": "┳",
-            "botl": "┡", "botr": "┩", "botsp": "╇",
-            "horz": "━", "vert": "┃"
-        }
-        section_header_top = {
-            "topl": "┣", "topr": "┫", "topsp": "┻",
+        section = {
             "botl": "┡", "botr": "┩", "botsp": "┯",
             "horz": "━", "vert": "┃"
         }
-        section_cell_top = {
-            "topl": "┢", "topr": "┪", "topsp": "┷",
-            "botl": "┡", "botr": "┩", "botsp": "┯",
-            "horz": "━", "vert": "┃"
-        }
-        cell_header_top = {
-            "topl": "┡", "topr": "┩", "topsp": "╇",
+        cell_a = {
             "botl": "├", "botr": "┤", "botsp": "┼",
             "horz": "─", "vert": "│"
         }
-        cell_section_top = {
-            "topl": "┡", "topr": "┩", "topsp": "┯",
-            "botl": "├", "botr": "┤", "botsp": "┼",
-            "horz": "─", "vert": "│"
-        }
-        cell_section_bot = {
-            "topl": "├", "topr": "┤", "topsp": "┼",
+        cell_b = {
             "botl": "┢", "botr": "┪", "botsp": "┷",
-            "horz": "─", "vert": "│"
+            "horz": "━", "vert": "│"
         }
-        cell_cell_bot = {
-            "topl": "├", "topr": "┤", "topsp": "┼",
-            "botl": "├", "botr": "┤", "botsp": "┼",
-            "horz": "─", "vert": "│"
-        }
-        cell_end = {
-            "topl": "├", "topr": "┤", "topsp": "┼",
+        cell_c = {
             "botl": "└", "botr": "┘", "botsp": "┴",
             "horz": "─", "vert": "│"
         }
 
-        if instance == "header_only":
-            border = header_only
-        elif instance == "header_section_bot":
-            border = header_section_bot
-        elif instance == "header_cell_bot":
-            border = header_cell_bot
-        elif instance == "section_header_top":
-            border = section_header_top
-        elif instance == "section_cell_top":
-            border = section_cell_top
-        elif instance == "section_cell_top":
-            border = section_cell_top
-        elif instance == "cell_header_top":
-            border = cell_header_top
-        elif instance == "cell_section_top":
-            border = cell_section_top
-        elif instance == "cell_section_bot":
-            border = cell_section_bot
-        elif instance == "cell_cell_bot":
-            border = cell_cell_bot
-        elif instance == "cell_end":
-            border = cell_end
+        if instance == "header":
+            border = header
+        elif instance == "section":
+            border = section
+        elif instance == "cell_a":
+            border = cell_a
+        elif instance == "cell_b":
+            border = cell_b
+        elif instance == "cell_c":
+            border = cell_c
         
         text_size = size - 2
-        if text == "":
-            mid = f"{border['vert']}{' ' * size}{border['vert']}"
+        if text == "" and pos != "start":
+            mid = f" {' ' * size}{border['vert']}"
         else:
-            if pos == "start":
+            if pos == "start" and instance != "section":
                 mid = f"{border['vert']} {self._center_text(text, text_size)} {border['vert']}"
-            else:
+            elif instance != "section":
                 mid = f" {self._center_text(text, text_size + 1)} {border['vert']}"
+        
+        if instance == "section":
+            if pos == "start":
+                mid = f"{border['vert']} {text}{' ' * (size-len(text))}"
+            if pos == "mid":
+                mid = f"{' ' * size}  "
+            if pos == "end":
+                mid = f"{" " * size} {border['vert']}"
 
-        if pos == "only":
-            top = border["topl"] + border["horz"] * size + border["topr"]
-            bot = border["botl"] + border["horz"] * size + border["botr"]
-        elif pos == "start":
-            top = border["topl"] + border["horz"] * size + border["topsp"]
+        if pos == "start":
+            if instance == "header":
+                top = border["topl"] + border["horz"] * size + border["topsp"]
             bot = border["botl"] + border["horz"] * size + border["botsp"]
         elif pos == "mid":
-            top = border["horz"] + border["horz"] * size + border["topsp"]
+            if instance == "header":
+                top = border["horz"] + border["horz"] * size + border["topsp"]
             bot = border["horz"] + border["horz"] * size + border["botsp"]
         elif pos == "end":
-            top = border["horz"] + border["horz"] * size + border["topr"]
+            if instance == "header":
+                top = border["horz"] + border["horz"] * size + border["topr"]
             bot = border["horz"] + border["horz"] * size + border["botr"]
         
-        return (top, mid, bot)
+        if instance == "header":
+            return (top, mid, bot)
+        else:
+            return ("", mid, bot)
     
     def _create_row(self, top_mid_bot: list, header_section_cell: str, size: int, pos: str, instance: str):
         temp_top, temp_mid, temp_bot = self._create_border(header_section_cell, size, pos, instance)
@@ -260,42 +207,23 @@ class TableBuilder:
                 if not next_cell:
                     return f"{current_cell}_end"
     
+    def save(self):
+        with open("schedule_table.txt", "w") as f:
+            f.write(self.build_table())
+
     def __str__(self):
-        return self.build_table()
+        return f"{self.build_table()}"
 
 test = TableBuilder(["", "9-11", "11-1", "1-2", "2-4", "4-6", "6-8"], "Test")
-test.add_row(["workroom"], True)
-test.add_row(["pick-up window", "Chris", "Sonaite", "Emaleigh", "Shawn", "Wil", "Deborah"])
+test.add_row(["workroom"], section=True)
+test.add_row(["pick-up window", "Jess", "Yami", "Chris", "Shawn", "Cheryl", "Deborah"])
+test.add_row(["floor lead", "Chris", "Janet", "", "Janet", "", "Sonaite"])
+test.add_row(["computer desk"], section=True)
+test.add_row(["service pt 1", "Janet", "Cat", "Jess", "Chris", "Wendy", "Cheryl"])
+test.add_row(["service pt 1", "", "Emaleigh", "", "Lea", "Emaleigh", "Shawn"])
+test.add_row(["staff/time permitting"], section=True)
+test.add_row(["meetings/programs", "", "", "", "", "", ""])
+test.add_row(["project time", "RF, AY, YE", "RF, LS, JB", "LS, EK, LT, CA", "RF, AY, YE, SDK, EK", "MD, SS; LS; RF, YE, JB, CW, LT, JH, AY", "MD, LT"])
 
 print(test)
-
-"""
-       TABLE
-┏━━━━━━━━┳━━━━━━━━┓
-┃        ┃        ┃
-┣━━━━━━━━┻━━━━━━━━┫
-┃                 ┃
-┡━━━━━━━━┯━━━━━━━━┩
-│        │        │
-├────────┼────────┤
-│        │        │
-┢━━━━━━━━┷━━━━━━━━┪
-┃                 ┃
-┡━━━━━━━━┯━━━━━━━━┩
-│        │        │
-├────────┼────────┤
-│        │        │
-└────────┴────────┘
-
-┏━━━━━━━━┳━━━━━━━━┓
-┃        ┃        ┃
-┡━━━━━━━━╇━━━━━━━━┩
-│        │        │
-┢━━━━━━━━┷━━━━━━━━┪
-┃                 ┃
-┡━━━━━━━━┯━━━━━━━━┩
-│        │        │
-├────────┼────────┤
-│        │        │
-└────────┴────────┘
-"""
+# test.save()
--- scratch.py
+++ scratch.py
@@ -1,95 +1,13 @@
-header_only = {
-    "topl": "┏", "topr": "┓", "topsp": "┳",
-    "botl": "┗", "botr": "┛", "botsp": "┻",
-    "horz": "━", "vert": "┃"
-}
-header_section_bot = {
-    "topl": "┏", "topr": "┓", "topsp": "┳",
-    "botl": "┣", "botr": "┫", "botsp": "┻",
-    "horz": "━", "vert": "┃"
-}
-header_cell_bot = {
-    "topl": "┏", "topr": "┓", "topsp": "┳",
-    "botl": "┡", "botr": "┩", "botsp": "╇",
-    "horz": "━", "vert": "┃"
-}
-section_header_top = {
-    "topl": "┣", "topr": "┫", "topsp": "┻",
-    "botl": "┡", "botr": "┩", "botsp": "┯",
-    "horz": "━", "vert": "┃"
-}
-section_cell_top = {
-    "topl": "┢", "topr": "┪", "topsp": "┷",
-    "botl": "┡", "botr": "┩", "botsp": "┯",
-    "horz": "━", "vert": "┃"
-}
-cell_header_top = {
-    "topl": "┡", "topr": "┩", "topsp": "╇",
-    "botl": "├", "botr": "┤", "botsp": "┼",
-    "horz": "─", "vert": "│"
-}
-cell_section_top = {
-    "topl": "┡", "topr": "┩", "topsp": "┯",
-    "botl": "├", "botr": "┤", "botsp": "┼",
-    "horz": "─", "vert": "│"
-}
-cell_section_bot = {
-    "topl": "├", "topr": "┤", "topsp": "┼",
-    "botl": "┢", "botr": "┪", "botsp": "┷",
-    "horz": "─", "vert": "│"
-}
-cell_end = {
-    "topl": "├", "topr": "┤", "topsp": "┼",
-    "botl": "└", "botr": "┘", "botsp": "┴",
-    "horz": "─", "vert": "│"
-}
+header = [["", "9-11", "11-1", "1-2", "2-4", "4-6", "6-8"]]
+section = [["", "", "", "", "", "", ""], ["workroom", "", "", "", "", "", ""], ["", "", "", "", "", "", ""], ["", "", "", "", "", "", ""]]
+cell = [["", "", "", "", "", "", ""], ["", "", "", "", "", "", ""], ["pick-up window", "Chris", "Sonaite", "Emaleigh", "Shawn", "Wil", "Deborah"]]
 
-instance = ""
+all_rows = header + section + cell
+num_cols = len(header[0])
 
-if instance == "header_only":
-    border = header_only
-elif instance == "header_section_bot":
-    border = header_section_bot
-elif instance == "header_cell_bot":
-    border = header_cell_bot
-elif instance == "section_header_top":
-    border = section_header_top
-elif instance == "section_cell_top":
-    border = section_cell_top
-elif instance == "section_cell_top":
-    border = section_cell_top
-elif instance == "cell_header_top":
-    border = cell_header_top
-elif instance == "cell_section_top":
-    border = cell_section_top
-elif instance == "cell_section_bot":
-    border = cell_section_bot
-elif instance == "cell_end":
-    border = cell_end
+widths = []
+for col in range(num_cols):
+    max_len = max((len(row[col]) for row in all_rows if col < len(row)), default=0)
+    widths.append(max(max_len, 10))
 
-def _get_cell_type(last_cell: str, current_cell: str, next_cell: str):
-    if not last_cell:
-        return f"{current_cell}_{next_cell}_bot"
-    if current_cell == "header":
-        return f"{current_cell}_only" if not next_cell else f"{current_cell}_{next_cell}_bot"
-    if current_cell == "section":
-        return f"{current_cell}_{last_cell}_top"
-    if current_cell == "cell":
-        if last_cell in {"header", "section"}:
-            return f"{current_cell}_{last_cell}_top"
-        if last_cell == "cell":
-            if next_cell == "section":
-                return f"{current_cell}_{next_cell}_bot"
-            if not next_cell:
-                return f"{current_cell}_end"
-
-instances = ["header", "section", "cell", "cell", "section", "cell", "cell"]
-
-for i in range(len(instances)):
-    last_cell, current_cell, next_cell = "", "", ""
-    current_cell = instances[i]
-    if i < len(instances) - 1:
-        next_cell = instances[i + 1]
-    if i > 0:
-        last_cell = instances[i - 1]
-    print(_get_cell_type(last_cell, current_cell, next_cell))
+print(widths)
